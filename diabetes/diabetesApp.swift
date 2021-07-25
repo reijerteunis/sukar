d896ff0990352943cfe1d30d86a4ac4e097fe411
Ruud van engelenhoven <info@appthena.com>
2021-07-25 10:05:42 +0400

    Initial Commit

---
+++ diabetes/diabetesApp.swift
@@ -0,0 +1,17 @@
+//
+//  diabetesApp.swift
+//  diabetes
+//
+//  Created by Ruud van Engelenhoven on 25/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct diabetesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
