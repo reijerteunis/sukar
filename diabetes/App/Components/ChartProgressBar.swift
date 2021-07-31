fc7a7c9b9f187b64de9e64a64f4c74c753ce6ac9
Ruud van engelenhoven <info@appthena.com>
2021-07-31 12:55:51 +0400

    Added dates to weekly view

---
+++ diabetes/App/Components/ChartProgressBar.swift
@@ -0,0 +1,20 @@
+//
+//  ChartProgressBar.swift
+//  diabetes
+//
+//  Created by Ruud van Engelenhoven on 29/07/2021.
+//
+
+import SwiftUI
+
+struct ChartProgressBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ChartProgressBar_Previews: PreviewProvider {
+    static var previews: some View {
+        ChartProgressBar()
+    }
+}
--- diabetes/App/View/Dashboard/DashboardWeeklyView.swift
+++ diabetes/App/View/Dashboard/DashboardWeeklyView.swift
@@ -8,6 +8,24 @@
 import SwiftUI
 
 struct DashboardWeeklyView: View {
+    let dayNameFormatter: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = .current
+        dateFormatter.calendar = .current
+        dateFormatter.dateFormat = "EE"
+        return dateFormatter
+    }()
+
+    let calDayFormatter: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = .current
+        dateFormatter.calendar = .current
+        dateFormatter.dateFormat = "dd"
+        return dateFormatter
+    }()
+
+    let last7Days = getDays()
+    
     var body: some View {
         BaseWidget (
             header: DashboardWidgetHeaderView(
@@ -16,8 +34,24 @@ struct DashboardWeeklyView: View {
                 iconColor: Color("BloodColor"),
                 chevron: false
             ),
-            content: HStack(spacing: 8,content: {
-                Text("Show widget")
+            content: HStack(content: {
+                ForEach(0..<last7Days.count) {index in
+                    VStack {
+                        Text(dayNameFormatter.string(from: last7Days[index]))
+                            .font(.system(size: 12))
+                            .foregroundColor(/*@START_MENU_TOKEN@*/Color("TextColor")/*@END_MENU_TOKEN@*/)
+                        ZStack {
+                            Circle()
+                                .frame(width: 37.0, height: 37.0)
+                                .foregroundColor(Color("BackgroundColor"))
+                            Text(calDayFormatter.string(from: last7Days[index]))
+                                .foregroundColor(/*@START_MENU_TOKEN@*/Color("TextColor")/*@END_MENU_TOKEN@*/)
+                        }
+                    }
+                    if (index != last7Days.count-1) {
+                        Spacer()
+                    }
+                }
             })
             .padding(.bottom, 16.0),
             footer: BaseWidgetFooterText(title: "We recommend you measure your blood sugar at least 3 times a day")
@@ -29,5 +63,21 @@ struct DashboardWeeklyView_Previews: PreviewProvider {
     static var previews: some View {
         DashboardWeeklyView()
             .previewLayout(.fixed(width: 380.0, height: 224))
+    }
+}
+
+// MARK: - getDays Function
+extension DashboardWeeklyView {
+    static func getDays() -> [Date] {
+        let cal = Calendar.current
+        var date = cal.startOfDay(for: Date())
+        var days = [Date]()
+
+        for _ in 1...7 {
+            date = cal.date(byAdding: Calendar.Component.day, value: -1, to: date)!
+            days.append(date)
+        }
+        days.reverse()
+        return days
     }
 }
